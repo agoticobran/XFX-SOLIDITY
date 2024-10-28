24bd0e965a5c98c25d25ee403ca392561ce0adf3
agoticobran <agoticobran@gmail.com>
2024-10-28 19:26:01 +0000

    Chapter 1 D

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -2,8 +2,11 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory{
 
-    uint dnaDigits = 16;
-    
-     uint dnaModulus = 10 ** dnaDigits;
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
 
+  truct Zombie{
+    string name;
+    uint dna;
+    }
 }
